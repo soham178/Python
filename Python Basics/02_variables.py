aa566503bb6596af7d7bf7dcf62220a8342838a0
soham178 <sohamsarkar552@gmail.com>
2026-07-12 20:06:48 +0530

    variables

---
+++ Python Basics/02_variables.py
@@ -0,0 +1,37 @@
+# Variables --> Reserved memory locations
+
+# 6x + 7y + 2z = 0 --> here x,y,z are variables which can have different kind of values 
+
+a = 1 # 1 is the value that is asigned to the 'a' variable
+
+print(a) # the result is 1
+
+b = 2
+print(b) # the result is 2
+
+c = 3
+print(c) # the result is 3
+
+d = a + b + c
+print(d)      # the result is 6
+
+del d # this is used to delete the variable
+# print(d)  gives an error 
+
+x = 5
+print(x) # the result is 5
+x = 3
+print(x) # the value of x replaced to 3 from 5
+
+a = "python"
+print(a) # the result is python
+
+
+# 5 = 1 --> this is wrong, you can't use number for variable name
+# age = 50
+# print(Age) --> gives an error because variables are case sensitive
+
+a1 = "Hello"
+a2 = " World"
+b = a1 + a2
+print(b) # the result is Hello World
