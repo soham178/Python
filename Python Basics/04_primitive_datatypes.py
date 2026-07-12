db0cd652ec9ef0cdfffe9f5907479fafcaa84346
soham178 <sohamsarkar552@gmail.com>
2026-07-12 20:54:16 +0530

    primitive data types

---
+++ Python Basics/04_primitive_datatypes.py
@@ -0,0 +1,50 @@
+# <---------PRIMITIVE DATATYPES---------->
+
+# Integers
+# Floats
+# Strings
+# Boolean
+# None
+
+
+# <-----Integers----->
+
+age = 25 # In python, we don not need to write the type of data like "int age = 25" (code in c language)
+
+print(type(age)) # used to check the data type --> result is <class 'int'>
+
+
+# <-----Floats----->
+
+percent = 87.3
+print(percent)
+print(type(percent)) # the result is <class 'float'>
+
+# 10 + 20 = 30 (integer + integer = integer)
+# 3.5 + 4.7 = 8.2 (float + float = float)
+# 4 + 3.5 = 7.5 (integer + float = float)
+
+
+# <-----Strings----->
+
+name = "Gaurav"
+print(name)
+print(type(name)) # the result is <class 'str'>
+
+full_name = "Gaurav Methe"
+print(full_name)
+print(type(full_name)) # the result is <class 'str'>
+
+
+# <-----Boolean----->
+
+val = True
+print(val)
+print(type(val)) # the result is <class 'bool'>
+
+
+# <-----None----->
+
+val1 = None
+print(val1)
+print(type(val1)) # the result is <class 'NoneType'>
