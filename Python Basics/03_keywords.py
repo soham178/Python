ac647daefbbf8af00d0560d78f1aabf0a20f4713
soham178 <sohamsarkar552@gmail.com>
2026-07-12 20:20:17 +0530

    keywords

---
+++ Python Basics/03_keywords.py
@@ -0,0 +1,5 @@
+# for, while, break, continue, def, lambda, and, or, not, True, False , None etc........
+
+# these are some keywords in python. All of them have their own purpose, you can't use them as a variable name.
+
+# this keywords comes when you install the python in your system
