549e5807379bf5df6494cc884e40ca13401d0dba
soham178 <sohamsarkar552@gmail.com>
2026-07-12 19:45:15 +0530

    literals notes

---
+++ Python Basics/01_Literals.py
@@ -0,0 +1,17 @@
+# 1000, -500, 0 --> Integer Literals --> used to store someone's age 
+
+# 3.14, 91.5, -5.89, 10.0 --> Floating Point Litereals -->used to store someone's marks 
+
+# 'Hello', "Hello", '''Hello''', """Hello"""  --> String Literal 
+
+# Hello, 'Hello" --> this are wrong declaration of string literals
+
+# "Hello everyone, we are learning Python" --> a sentence can also be a string literals
+
+# """Hello,
+# How are you?
+# Let's learn Python""" --> if you want create multiline string "", '' will not work, you have to use ''' ''', """ """
+
+# True, Flase --> Boolean Literals --> use to check certain conditions (case sensitive)
+
+# None --> Special Literal --> used when you don't know the value (case sensitive)
