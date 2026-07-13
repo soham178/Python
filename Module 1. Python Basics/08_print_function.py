0d6a268af0d32185334b2b755300a30cb6183926
soham178 <sohamsarkar552@gmail.com>
2026-07-13 20:36:23 +0530

    print function

---
+++ Module 1. Python Basics/08_print_function.py
@@ -0,0 +1,29 @@
+num1 = 100
+print(num1) #100
+
+name = "John"
+print(name) # John
+age = 20
+print(age) # 20
+print(name, age) # John 20
+
+print(10,20,30,40) # 10 20 30 40
+
+print(10,20,30,40, sep=" ") # 10 20 30 40 (default)
+print(10,20,30,40, sep=",") # 10,20,30,40
+print(10,20,30,40, sep="#") # 10#20#30#40
+
+num1 = 100
+num2 = 200
+result = num1 + num2
+print("Addition of",num1,"and",num2,"is",result)
+# Addition of 100 and 200 is 300
+
+print("Name:",name,"Age:",age)
+# Name: John Age: 20
+
+day = 13
+month = 7
+year = 2026
+
+print(day,month,year, sep="/")
