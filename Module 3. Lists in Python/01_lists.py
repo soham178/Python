0f0bae951646dd8fa5aa7d585c1827896de689bf
soham178 <sohamsarkar552@gmail.com>
2026-07-14 20:31:23 +0530

    lists introduction

---
+++ Module 3. Lists in Python/01_lists.py
@@ -0,0 +1,16 @@
+name = "John"
+age = 20
+percent = 85.5
+
+student = ["John", 20, 85.5]
+print(type(student)) # <class 'list'>
+print(student) # ['John', 20, 85.5]
+
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+print(days_of_week[0]) # Mon
+print(days_of_week[4]) # Fri
+print(f"Last day of the week is {days_of_week[-1]}")
+# Last day of the week is Sun
+
+# Length of a list => the number of items/elements in the list
+print(len(days_of_week)) # 7
