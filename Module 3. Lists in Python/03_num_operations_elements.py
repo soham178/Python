e36b4b0b6f281659b5c95919f22ad24b38e887bc
soham178 <sohamsarkar552@gmail.com>
2026-07-14 22:54:58 +0530

    Numerical operations on list elements

---
+++ Module 3. Lists in Python/03_num_operations_elements.py
@@ -0,0 +1,16 @@
+numbers = [10, 4, -1, 20, 5.5, 7, 1]
+
+# smallest number in the list
+# min()
+# print(numbers.min()) # AttributeError: 'list' object has no attribute 'min'
+print(min(numbers)) # -1
+
+
+# Biggest number in the list
+# max()
+print(max(numbers)) # 20
+
+
+# Total of the numbers in the list
+# sum()
+print(sum(numbers)) # 46.5
